34caa5104e8a1934814b0f2350dcc404d3a8e5a8
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-06-16 01:57:44 +0530

    remove scheduler and refactor code

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 const scheduler = require('node-schedule');
 const PIPE = require('./main.js');
 
-const job = scheduler.scheduleJob('5 0 * * *', PIPE.main);
-console.log("job added\nNew birthdays will be checked everyday at 00:05!");
-
-
-while (true){
-}
+PIPE.main();
