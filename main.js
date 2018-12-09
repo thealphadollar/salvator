e9055fcc260e85fc7ad5929e5a3ee4a44b59c098
“raydwaipayan” <dwaipayanray1@gmail.com>
2018-12-09 22:36:28 +0530

    Revert "No birthdays fixed"
    This reverts commit 3fbce37d831e5f89544745f5f1e630257eb52622.

--- main.js
+++ main.js
@@ -110,14 +110,7 @@ async function main() {
     const names = await resolveData.resolveNames(birthday_data.raw_names);
     // console.log(links.messageLinks);
     console.log('data fetched');
-	
-	if (links.messageLinks.length==0)
-	{
-		console.log("No Birthdays found. Exiting in 3 seconds...");
-		await page.waitFor(3000);
-		await browser.close();
-		return;
-	}
+
     if (links.messageLinks.length === names.firstNames.length){
         console.log('sending messages with names');
         await initMessages(browser, links.messageLinks, names.firstNames);
