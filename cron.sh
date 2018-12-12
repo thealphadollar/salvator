a4c89122b466bfbe08a0a4cbc44907a59ad723b0
“raydwaipayan” <dwaipayanray1@gmail.com>
2018-12-12 18:41:07 +0530

    Cronjob installation check

--- cron.sh
+++ cron.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #Ask the user to add or remove the cron job
 
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' cron|grep "install ok installed")
+echo "Checking for cron: $PKG_OK"
+if [ "" == "$PKG_OK" ]; then
+  echo "Cron not installed. Setting up cron"
+  sudo apt-get --force-yes --yes install cron
+fi
+
+
 echo "Press 1 to add a new cron job, 2 to delete existing or 3 te view all cron jobs"
 read op
 if [ $op -eq 1 ];then 
