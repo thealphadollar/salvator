c3cc91424956398f469e88a1ad994718499062f3
Minanshu Singh <minanshu.1998@gmail.com>
2018-12-14 21:01:47 +0530

    working cli version@0.1.0

--- cli.js
+++ cli.js
@@ -5,69 +5,120 @@ const inquirer = require("inquirer");
 const chalk = require("chalk");
 const execa = require("execa");
 const Listr = require("listr");
-const input = require("listr-input");
 const fs = require("fs");
-const Observable = require('rxjs').Observable;
-const through = require('through');
+const path = require("path");
+const logSymbols = require("log-symbols");
 
 const pjson = require("./package.json");
-const {env_query} = require('./questions_cli.js')
+const { env_query } = require("./questions_cli.js");
 
+const PATH_TO_DOTENV = path.resolve(__dirname, '.env');
+const PATH_TO_INDEX = path.resolve(__dirname, 'index.js');
 
+program
+  .version(pjson.version, "-v --version")
+  .description(chalk.yellowBright.bold(pjson.description));
 
+program
+  .command("env")
+  .description(
+    chalk.blue(
+      "Load environment variables in .env file(fb_id, fb_pass, email, ..)"
+    )
+  )
+  .action(() => {
+    inquirer.prompt(env_query).then(env_values => {
+      const file = fs.createWriteStream(PATH_TO_DOTENV);
+      for (let property in env_values) {
+        file.write(`${property}=${env_values[property]}\n`);
+      }
+      file.end();
+      console.log(
+        chalk.green.bold(
+          "\n--------------------------------------------------------------\n"
+        )
+      );
+      console.log(
+        logSymbols.success,
+        chalk.green.bold("Environment variables loaded in .env file\n")
+      );
+    });
+  });
 
-const tasks = new Listr([
-  /*{
-    title: "Install package dependencies with npm",
-    //enabled: ctx => ctx.yarn === false,
-    task: () => execa("npm", ["install"])
-  },
-  ,*/
-  {
-    title: "Enter environment variables",
-    task: () => {
-      return new Observable(observer => {
-        let buffer = "";
-
-        const outputStream = through(data => {
-          if (/\u001b\[.*?(D|C)$/.test(data)) {
-            if (buffer.length > 0) {
-              observer.next(buffer);
-              buffer = "";
-            }
-            return;
+program
+  .command("run")
+  .description(chalk.blue("Run the automated Birthday wish script"))
+  .action(() => {
+    console.log("Running index.js");
+    /*
+    1. Check if .env file exists or not
+    2. run node index.js
+    */
+    const tasks = new Listr([
+      {
+        title: "Checking the presence of .env file",
+        task: (ctx, task) => {
+          if (fs.existsSync(PATH_TO_DOTENV)) {
+            ctx.env = true;
+          } else {
+            ctx.env = false;
+            task.skip(
+              `Please create .env file by running 'puppeteer-salvator env'`
+            );
           }
+        }
+      },
+      {
+        title: "Running index.js",
+        enabled: ctx => ctx.env,
+        task: async () => {
+          try {
+            const stream = await execa("node", [PATH_TO_INDEX]).stdout;
+            stream.pipe(process.stdout);
+          } catch (err) {
+            console.log(`Error: ${err}`);
+          }
+        }
+      }
+    ]);
 
-          buffer += data;
-        });
+    tasks.run().catch(err => console.error(err));
+  });
 
-        const prompt = inquirer.createPromptModule({
-          output: outputStream
-        });
+program
+  .command("*")
+  .description(chalk.blue("When no matching commands are entered"))
+  .action(function(env) {
+    console.log(logSymbols.warning, chalk.red(" Command not available"));
+    console.log(chalk.blue("Type --help to see all the available commands"));
+  });
 
-        prompt(env_query)
-          .then(env_values => {
-            observer.next(); // Clear the output
-						const file = fs.createWriteStream('.log');
-						for(let property in env_values) {
-							file.write(`${property}:${env_values[property]}\n`);
-						}
-						file.end();
-					})
-					.then(() => {
-						observer.complete();
-					})
-          .catch(err => {
-            observer.error(err);
-					});
-        return outputStream;
-      });
-    }
-	},
-	{
-    title: "Running index.js",
-    task: () => execa("node", ["index.js"])
-  }
-]);
+program.parse(process.argv);
 
-tasks.run().catch(err => console.error(err));
+// when no arguments are specified
+const NO_COMMAND_SPECIFIED = program.args.length === 0;
+if (NO_COMMAND_SPECIFIED) {
+  console.log("\n\n");
+  console.log(
+    chalk.yellow.bold(
+      "---------------------------------------------------------"
+    )
+  );
+  console.log(
+    chalk.greenBright.bold(`\n                   PUPPETEER SALVATOR\n`)
+  );
+  console.log(
+    chalk.yellow.bold(
+      "---------------------------------------------------------"
+    )
+  );
+  program.help();
+}
+
+/*
+Steps to test this cli-tool locally:
+1. npm link
+2. open terminal, run 'puppeteer-salvator'
+3. when done checking, run 'npm unlink'
+4. (for the owner of this repo) : run 'npm publish' to publish this as an npm cli-tool on npmjs.com
+*/
